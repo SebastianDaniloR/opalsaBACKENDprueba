10bdece7fcabf70781f64053d10457477934558e
unknown <sebastiandanilo19@gmail.com>
2024-10-28 11:49:44 -0500

    eliminamos el samesitesnone de auth controller

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,8 +1,9 @@
-import User from '../models/user.model.js';
-import bcrypt from 'bcryptjs';
+import  User from '../models/user.model.js'
+import bcrypt from 'bcryptjs'
 import { createAccessToken } from '../libs/jwt.js';
 import jwt from 'jsonwebtoken';
 import { TOKEN_SECRET } from '../config.js';
+
 
 export const register = async (req, res) => {
   const { email, password, username, cedula, cargo, ciudad, role = 'user' } = req.body;
@@ -41,12 +42,7 @@ export const register = async (req, res) => {
     const token = await createAccessToken({ id: userSaved._id });
     console.log('Token creado:', token);
 
-    // Configurar la cookie con SameSite=None y Secure=true (si está en HTTPS)
-    res.cookie("token", token, {
-      sameSite: 'None',
-      secure: true, // Cambiar a false si no estás en HTTPS
-      httpOnly: true, // Recomendado para proteger la cookie
-    });
+    res.cookie("token", token);
 
     res.json({
       id: userSaved._id,
@@ -65,62 +61,52 @@ export const register = async (req, res) => {
   }
 };
 
-export const login = async (req, res) => {
-  const { email, password } = req.body;
-  try {
-    const userFound = await User.findOne({ email });
-    if (!userFound) return res.status(400).json({ message: "user not found" });
 
-    const isMatch = await bcrypt.compare(password, userFound.password);
-    if (!isMatch) return res.status(400).json({ message: "incorrect password" });
+export const login = async (req,res) =>  {
+  const{email,password} = req.body;
+try {
+const userFound = await User.findOne({email})
+if(!userFound) return res.status(400).json({message: "user not found "});
 
-    const token = await createAccessToken({ id: userFound._id });
-
-    // Configurar la cookie con SameSite=None y Secure=true (si está en HTTPS)
-    res.cookie("token", token, {
-      sameSite: 'None',
-      secure: true, // Cambiar a false si no estás en HTTPS
-      httpOnly: true, // Recomendado para proteger la cookie
-    });
-
+const isMatch = await bcrypt.compare(password,userFound.password) 
+if (!isMatch) return res.status(400).json({message:"incorrect password"});
+    const token = await createAccessToken({id:userFound._id})
+    res.cookie("token",token)
     res.json({
-      id: userFound._id,
-      username: userFound.username,
-      email: userFound.email,
-      role: userFound.role,
-      createdAt: userFound.createdAt,
-      updatedAt: userFound.updatedAt,
-    });
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
+        id: userFound._id,
+        username: userFound.username,
+        email: userFound.email,
+        role: userFound.role,
+        createdAt: userFound.createdAt,
+        updateAt: userFound.updatedAt,
+})   
+} catch (error) {
+    res.status(500).json({message: error.message});
+}
 };
 
-export const logout = (req, res) => {
-  res.cookie('token', "", {
-    expires: new Date(0),
-    sameSite: 'None',
-    secure: true, // Cambiar a false si no estás en HTTPS
+export const logout = (req,res) =>{
+  res.cookie('token',"",{
+    expires: new Date(0)
   });
   return res.sendStatus(200);
 };
 
-export const profile = async (req, res) => {
-  const userFound = await User.findById(req.user.id);
-  if (!userFound) return res.status(400).json({ message: "user not found" });
-  return res.json({
-    id: userFound._id,
-    username: userFound.username,
-    email: userFound.email,
-    cedula: userFound.cedula,
-    cargo: userFound.cargo,
-    ciudad: userFound.ciudad,
-    role: userFound.role,
-    createdAt: userFound.createdAt,
-    updatedAt: userFound.updatedAt,
-  });
+export const profile = async (req, res) =>{
+ const userFound = await User.findById(req.user.id)
+ if(!userFound) return res.status(400).json({message:"user not found"});
+ return res.json({
+        id: userFound._id,
+        username: userFound.username,
+        email: userFound.email,
+        cedula: userFound.cedula,
+        cargo: userFound.cargo,
+        ciudad: userFound.ciudad,
+        role:userFound.role,
+        createdAt: userFound.createdAt,
+        updateAt: userFound.updatedAt,
+ })
 };
-
 export const getUserCount = async (req, res) => {
   try {
     const userCount = await User.countDocuments();
@@ -129,6 +115,7 @@ export const getUserCount = async (req, res) => {
     res.status(500).json({ message: error.message });
   }
 };
+
 
 export const verifyToken = async (req, res) => {
   try {
