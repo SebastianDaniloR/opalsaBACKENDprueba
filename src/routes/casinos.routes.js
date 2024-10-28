1dbacd3770cb1d3f7fc476ed4998e524998b71f1
unknown <sebastiandanilo19@gmail.com>
2024-10-28 11:34:46 -0500

    Elimino Auth Required

--- src/routes/casinos.routes.js
+++ src/routes/casinos.routes.js
@@ -13,9 +13,9 @@ import { getCasinoElementos } from "../controllers/casinos.controller.js";
 import { createCasinoSchema } from "../schemas/casinos.schema.js";
 const router = Router();
 
-router.get("/casinos", authRequired, getAllCasinos);
-router.get("/casinos/:id", authRequired, getCasinoById);
-router.get("/casinos/:id/elementos", authRequired, getCasinoElementos);
+router.get("/casinos", getAllCasinos);
+router.get("/casinos/:id", getCasinoById);
+router.get("/casinos/:id/elementos", getCasinoElementos);
 router.post(
   "/casinos",
   authRequired,
--- src/routes/mantenimiento.routes.js
+++ src/routes/mantenimiento.routes.js
@@ -1,10 +1,9 @@
 import {Router} from 'express';
-import { authRequired } from "../middlewares/validateToken.js";
 import { getMantenimientos, createMantenimiento, deleteMantenimiento  } from '../controllers/mantenimiento.controller.js'; // Ajusta las rutas de los controladores según tu estructura
 
 const router = Router();
 
-router.get('/mantenimientos',authRequired ,getMantenimientos);
+router.get('/mantenimientos', getMantenimientos);
 router.post('/mantenimientos', createMantenimiento);
 router.delete('/mantenimientos/:id', deleteMantenimiento);
 
